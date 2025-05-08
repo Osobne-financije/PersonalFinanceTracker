b3ceb3a4dc3505063eb9047dad922114244364de
bRoxorg <janbiro2003@gmail.com>
2025-05-08 16:51:21 +0200

    Povezivanje registracije sa bazom

--- Software/Osobne_Financije/Osobne_Financije/FrmSignup.Designer.cs
+++ Software/Osobne_Financije/Osobne_Financije/FrmSignup.Designer.cs
@@ -31,6 +31,18 @@
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.txtUName = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtCPass = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -40,7 +52,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label1.Location = new System.Drawing.Point(280, 38);
+            this.label1.Location = new System.Drawing.Point(280, 21);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(227, 32);
             this.label1.TabIndex = 1;
@@ -50,10 +62,10 @@
             // 
             this.pictureBox2.BackColor = System.Drawing.SystemColors.HotTrack;
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.pictureBox2.Image = global::Osobne_Financije.Properties.Resources.Untitled_design2;
-            this.pictureBox2.Location = new System.Drawing.Point(59, 207);
+            this.pictureBox2.Image = global::Osobne_Financije.Properties.Resources.Untitled_design__2_;
+            this.pictureBox2.Location = new System.Drawing.Point(70, 197);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(134, 107);
+            this.pictureBox2.Size = new System.Drawing.Size(101, 88);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
@@ -67,11 +79,137 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Window;
+            this.label4.Location = new System.Drawing.Point(12, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(214, 99);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "OSOBNE FINANCIJE";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label2.Location = new System.Drawing.Point(283, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Ime";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label3.Location = new System.Drawing.Point(283, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 15);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Prezime";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label5.Location = new System.Drawing.Point(283, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 15);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Korisničko ime";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label6.Location = new System.Drawing.Point(283, 205);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 15);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Lozinka";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label7.Location = new System.Drawing.Point(283, 258);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(103, 15);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Potvrdi lozinku";
+            // 
+            // txtFName
+            // 
+            this.txtFName.Location = new System.Drawing.Point(286, 83);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(150, 22);
+            this.txtFName.TabIndex = 16;
+            // 
+            // txtLName
+            // 
+            this.txtLName.Location = new System.Drawing.Point(286, 128);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(150, 22);
+            this.txtLName.TabIndex = 17;
+            // 
+            // txtUName
+            // 
+            this.txtUName.Location = new System.Drawing.Point(286, 172);
+            this.txtUName.Name = "txtUName";
+            this.txtUName.Size = new System.Drawing.Size(150, 22);
+            this.txtUName.TabIndex = 18;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(286, 224);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(150, 22);
+            this.txtPass.TabIndex = 19;
+            // 
+            // txtCPass
+            // 
+            this.txtCPass.Location = new System.Drawing.Point(286, 277);
+            this.txtCPass.Name = "txtCPass";
+            this.txtCPass.Size = new System.Drawing.Size(150, 22);
+            this.txtCPass.TabIndex = 20;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnRegister.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.Location = new System.Drawing.Point(315, 312);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(136, 36);
+            this.btnRegister.TabIndex = 22;
+            this.btnRegister.Text = "REGISTRACIJA";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
             // FrmSignup
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(519, 360);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.txtCPass);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUName);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.txtFName);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
@@ -92,5 +230,17 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.TextBox txtUName;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtCPass;
+        private System.Windows.Forms.Button btnRegister;
     }
 }
--- Software/Osobne_Financije/Osobne_Financije/FrmSignup.cs
+++ Software/Osobne_Financije/Osobne_Financije/FrmSignup.cs
@@ -1,20 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Osobne_Financije.Models;
+using Osobne_Financije.Repositories;
+using System;
 using System.Windows.Forms;
 
 namespace Osobne_Financije
 {
-    public partial class FrmSignup: Form
+    public partial class FrmSignup : Form
     {
         public FrmSignup()
         {
             InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            string firstName = txtFName.Text.Trim();
+            string lastName = txtLName.Text.Trim();
+            string username = txtUName.Text.Trim();
+            string password = txtPass.Text;
+            string confirmPassword = txtCPass.Text;
+
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(confirmPassword))
+            {
+                MessageBox.Show("Molimo ispunite sva polja.");
+                return;
+            }
+            if (password != confirmPassword)
+            {
+                MessageBox.Show("Lozinke se ne podudaraju.");
+                return;
+            }
+
+            Student novi = new Student
+            {
+                Firstname = firstName,
+                Lastname = lastName,
+                Username = username,
+                Password = password
+            };
+
+            StudentRepository studentRepository = new StudentRepository();
+            bool isRegistered = studentRepository.Signup(novi);
+
+            if (isRegistered)
+            {
+                MessageBox.Show("Registracija uspješna!");
+                this.Hide();
+                FrmLogin loginForm = new FrmLogin();
+                loginForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Registracija nije uspjela. Pokušajte ponovo.");
+            }
         }
     }
 }
