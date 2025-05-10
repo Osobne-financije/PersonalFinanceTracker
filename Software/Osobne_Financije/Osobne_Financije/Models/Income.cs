09938a9f7cae0b8da52268afc0be2a6f5cb2b525
bRoxorg <janbiro2003@gmail.com>
2025-05-10 15:50:12 +0200

    Dodana klasa Income

---
+++ Software/Osobne_Financije/Osobne_Financije/Models/Income.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osobne_Financije.Models
+{
+    class Income
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public int CategoryId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+
+    }
+}
