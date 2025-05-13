c62fa0999e6cfa25e1828fdbf032cd03c62f9c76
bRoxorg <janbiro2003@gmail.com>
2025-05-13 23:00:22 +0200

    napravljen CategoryRepositori
    Dodane metode GetCategoriesByType i AddCategory

---
+++ Software/Osobne_Financije/Osobne_Financije/Repositories/CategoryRepository.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using DBLayer;
+using System.Data.SqlClient;
+using Osobne_Financije.Models;
+
+namespace Osobne_Financije.Repositories
+{
+    class CategoryRepository
+    {
+        public List<Category> GetCategoriesByType(string type)
+        {
+            List<Category> categories = new List<Category>();
+
+            DB.OpenConnection();
+            string query = "SELECT * FROM Categories WHERE Type = '{type}'";
+            SqlDataReader reader = DB.GetDataReader(query);
+
+            while (reader.Read())
+            {
+                Category category = new Category
+                {
+                    Id = (int)reader["id"],
+                    Name = reader["Name"].ToString(),
+                    Type = reader["Type"].ToString(),
+                };
+                categories.Add(category);
+            }
+            DB.CloseConnection();
+            return categories;
+        }
+
+        public bool AddCategory(string name, string type)
+        {
+            DB.OpenConnection();
+            string query = $"INSERT INTO Categories (Name, Type) VALUES ('{name}', '{type}')";
+            int affectedRows = DB.ExecuteCommand(query);
+            DB.CloseConnection();
+            return affectedRows > 0;
+        }
+    }
+}
