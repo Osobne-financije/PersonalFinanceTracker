5f2b5c78c45d5e237ce70669efcd5137dc725e60
bRoxorg <janbiro2003@gmail.com>
2025-05-14 22:49:46 +0200

    Kreiran IncomeRepo i dodana metoda AddIncome

---
+++ Software/Osobne_Financije/Osobne_Financije/Repositories/IncomeRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using DBLayer;
+using Osobne_Financije.Models;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osobne_Financije.Repositories
+{
+    class IncomeRepository
+    {
+        public bool AddIncome(Income income)
+        {
+            DB.OpenConnection();
+
+            string query = $"INSERT INTO Income (StudentId, CategoryId, Amount, Date, Description) VALUES ({income.StudentId}, {income.CategoryId}, {income.Amount.ToString(System.Globalization.CultureInfo.InvariantCulture)}, '{income.Date:yyyy-MM-dd}', '{income.Description}')";
+            
+            int affectedRows = DB.ExecuteCommand(query);
+            DB.CloseConnection();
+
+            return affectedRows > 0;
+        }
+
+    }
+}
