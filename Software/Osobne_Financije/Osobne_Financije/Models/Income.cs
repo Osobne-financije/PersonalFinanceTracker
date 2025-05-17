8a1fe4a0f96c127c67db0945daf933c222f8881f
bRoxorg <janbiro2003@gmail.com>
2025-05-17 20:48:14 +0200

    Dodan CategoryName u klasu Income

--- Software/Osobne_Financije/Osobne_Financije/Models/Income.cs
+++ Software/Osobne_Financije/Osobne_Financije/Models/Income.cs
@@ -14,6 +14,7 @@ namespace Osobne_Financije.Models
         public decimal Amount { get; set; }
         public DateTime Date { get; set; }
         public string Description { get; set; }
+        public string CategoryName { get; set; }
 
     }
 }
