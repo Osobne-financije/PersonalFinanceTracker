a5ae55743fe676599975d338e7d858dc84e95a60
bRoxorg <janbiro2003@gmail.com>
2025-05-20 23:07:25 +0200

    Dodana funkcija ShowSummary za prikaz prihoda, rashoda i balansa

--- Software/Osobne_Financije/Osobne_Financije/FrmMain.Designer.cs
+++ Software/Osobne_Financije/Osobne_Financije/FrmMain.Designer.cs
@@ -28,19 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label6 = new System.Windows.Forms.Label();
+            this.lblTotalExpenses = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label5 = new System.Windows.Forms.Label();
+            this.lblTotalIncomes = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.panel4 = new System.Windows.Forms.Panel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -51,6 +49,8 @@
             this.lblIncomes = new System.Windows.Forms.Label();
             this.panel5 = new System.Windows.Forms.Panel();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -64,26 +64,26 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Red;
-            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.lblTotalExpenses);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Location = new System.Drawing.Point(303, 124);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(240, 100);
             this.panel1.TabIndex = 6;
             // 
-            // label6
+            // lblTotalExpenses
             // 
-            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.lblTotalExpenses.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.SystemColors.Window;
-            this.label6.Location = new System.Drawing.Point(100, 56);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(32, 24);
-            this.label6.TabIndex = 10;
-            this.label6.Text = "00";
+            this.lblTotalExpenses.AutoSize = true;
+            this.lblTotalExpenses.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalExpenses.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblTotalExpenses.Location = new System.Drawing.Point(100, 56);
+            this.lblTotalExpenses.Name = "lblTotalExpenses";
+            this.lblTotalExpenses.Size = new System.Drawing.Size(32, 24);
+            this.lblTotalExpenses.TabIndex = 10;
+            this.lblTotalExpenses.Text = "00";
             // 
             // label4
             // 
@@ -101,26 +101,27 @@
             this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.panel2.BackColor = System.Drawing.Color.LimeGreen;
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.lblTotalIncomes);
             this.panel2.Controls.Add(this.label3);
             this.panel2.Location = new System.Drawing.Point(1069, 124);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(240, 100);
             this.panel2.TabIndex = 7;
             // 
-            // label5
+            // lblTotalIncomes
             // 
-            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.lblTotalIncomes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.SystemColors.Window;
-            this.label5.Location = new System.Drawing.Point(99, 55);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(32, 24);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "00";
+            this.lblTotalIncomes.AutoSize = true;
+            this.lblTotalIncomes.BackColor = System.Drawing.Color.LimeGreen;
+            this.lblTotalIncomes.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalIncomes.ForeColor = System.Drawing.SystemColors.Window;
+            this.lblTotalIncomes.Location = new System.Drawing.Point(99, 55);
+            this.lblTotalIncomes.Name = "lblTotalIncomes";
+            this.lblTotalIncomes.Size = new System.Drawing.Size(32, 24);
+            this.lblTotalIncomes.TabIndex = 9;
+            this.lblTotalIncomes.Text = "00";
             // 
             // label3
             // 
@@ -141,44 +142,24 @@
             this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel3.BackColor = System.Drawing.SystemColors.Menu;
-            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.label2);
             this.panel3.Controls.Add(this.label11);
-            this.panel3.Controls.Add(this.textBox2);
+            this.panel3.Controls.Add(this.label1);
             this.panel3.Location = new System.Drawing.Point(262, -1);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(1086, 87);
             this.panel3.TabIndex = 17;
-            // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 13.2F, System.Drawing.FontStyle.Bold);
-            this.label1.ForeColor = System.Drawing.SystemColors.MenuText;
-            this.label1.Location = new System.Drawing.Point(743, 31);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(196, 26);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Financijsko stanje";
             // 
             // label11
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold);
             this.label11.ForeColor = System.Drawing.SystemColors.MenuText;
-            this.label11.Location = new System.Drawing.Point(35, 24);
+            this.label11.Location = new System.Drawing.Point(67, 26);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(161, 35);
             this.label11.TabIndex = 1;
             this.label11.Text = "POČETNA";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox2.Location = new System.Drawing.Point(958, 35);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 0;
             // 
             // panel4
             // 
@@ -307,20 +288,48 @@
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
             this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
             this.chart1.Location = new System.Drawing.Point(0, 0);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart1.Series.Add(series6);
             this.chart1.Size = new System.Drawing.Size(1006, 333);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Menu;
+            this.label1.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.label1.Location = new System.Drawing.Point(657, 26);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(10, 8, 10, 7);
+            this.label1.Size = new System.Drawing.Size(245, 42);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Financijsko stanje:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Yellow;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label2.Location = new System.Drawing.Point(894, 24);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(10);
+            this.label2.Size = new System.Drawing.Size(119, 44);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "1500.23 €";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FrmMain
             // 
@@ -337,6 +346,7 @@
             this.Name = "FrmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Početna";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
@@ -358,12 +368,10 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTotalExpenses;
+        private System.Windows.Forms.Label lblTotalIncomes;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Label lblReports;
         private System.Windows.Forms.Label lblExpenses;
@@ -374,5 +382,7 @@
         private System.Windows.Forms.Button button;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Software/Osobne_Financije/Osobne_Financije/FrmMain.cs
+++ Software/Osobne_Financije/Osobne_Financije/FrmMain.cs
@@ -1,11 +1,8 @@
-﻿using System;
+﻿using Osobne_Financije.Models;
+using Osobne_Financije.Repositories;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Osobne_Financije
@@ -15,6 +12,11 @@ namespace Osobne_Financije
         public FrmMain()
         {
             InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            ShowSummary();
         }
 
         private void lblIncomes_Click(object sender, EventArgs e)
@@ -49,5 +51,22 @@ namespace Osobne_Financije
             lblExpenses.Location = new Point(lblExpenses.Location.X + 5, lblExpenses.Location.Y);
             lblExpenses.Font = new Font(lblExpenses.Font.FontFamily, 11, FontStyle.Bold);
         }
+
+        private void ShowSummary()
+        {
+            var incomeRepo = new IncomeRepository();
+            var expenseRepo = new ExpenseRepository();
+
+            int studentId = Session.LoggedStudent.Id;
+
+            decimal totalIncome = incomeRepo.GetTotalIncomeByStudentId(studentId);
+            decimal totalExpense = expenseRepo.GetTotalExpenseByStudentId(studentId);
+            decimal balance = totalIncome - totalExpense;
+
+            lblTotalIncomes.Text = totalIncome.ToString("0.00") + " €";
+            lblTotalExpenses.Text = totalExpense.ToString("0.00") + " €";
+            label2.Text = balance.ToString("0.00") + " €";
+        }
+
     }
 }
