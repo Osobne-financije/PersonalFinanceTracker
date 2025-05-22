1fd57370950bb6d8ec4dec7c57e99fe93e52b9cb
bRoxorg <janbiro2003@gmail.com>
2025-05-22 21:07:30 +0200

    Kreirana klasa CategoryGoal

---
+++ Software/Osobne_Financije/Osobne_Financije/Models/CategoryLimit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osobne_Financije.Models
+{
+    class CategoryLimit
+    {
+        public int LimitId { get; set; }
+        public int StudentId { get; set; }
+        public int CategoryId { get; set; }
+        public decimal LimitAmount { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
