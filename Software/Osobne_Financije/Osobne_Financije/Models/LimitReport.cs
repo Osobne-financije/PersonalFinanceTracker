1e397a54c1abbe69f6caa65afd12222d2737ab7d
bRoxorg <janbiro2003@gmail.com>
2025-05-23 15:38:42 +0200

    kreirana klasa LimiReport

---
+++ Software/Osobne_Financije/Osobne_Financije/Models/LimitReport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osobne_Financije.Models
+{
+    class LimitReport
+    {
+        public string CategoryName { get; set; }
+        public decimal LimitAmount { get; set; }
+        public decimal Spent { get; set; }
+
+        public decimal Remaining
+        {
+            get
+            {
+                return LimitAmount - Spent;
+            }
+        }
+    }
+}
